9d855bb9b45a804f0813b3c1a0b87854ea645dcb
QuentinLec <quentin.lecourt42@gmail.com>
2014-11-13 12:13:41 +0100

    Commit test 3

--- src/com/studio124/zurvivor/MenuCreationCompte.java
+++ src/com/studio124/zurvivor/MenuCreationCompte.java
@@ -21,7 +21,7 @@ public class MenuCreationCompte extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu_creation_compte);
-		String test = "test"; 
+		String test = "test!!!!!!!!!!!!!!!"; 
 		dataSource = new dataSource(this);
 		dataSource.open();
 		
